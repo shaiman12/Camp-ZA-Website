65c8eb93784af1d28e3136fefc8d766607833254
Shai Aarons <shai.aarons21@gmail.com>
2020-04-26 08:37:23 +0200

    Shouldve gitted way earlier. Anyway I just refactored my  code

---
+++ routes/campgrounds.js
@@ -0,0 +1,56 @@
+var express                 = require("express");
+var router                  = express.Router();
+var Campground              = require("../models/campground");
+
+//INDEX ROUTE
+router.get("/",function(req,res){
+
+    Campground.find({},function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
+        }
+    });
+});
+
+//CREATE ROUTE
+router.post("/", function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {
+        name: name,
+        image: image,
+        description: desc
+    }
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+
+});
+
+//NEW ROUTE
+router.get("/new", function(req,res){
+    res.render("campgrounds/new");
+});
+
+//SHOW ROUTE
+router.get("/:id", function(req, res){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+    
+    
+});
+
+
+module.exports = router;
