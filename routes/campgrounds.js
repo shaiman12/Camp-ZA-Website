37274e6b9728a6e0d70601716c16425cdf54fda9
Shai Aarons <shai.aarons21@gmail.com>
2020-04-26 10:15:13 +0200

    Added edit and update routes + edit button on show page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,6 +57,30 @@ router.get("/:id", function(req, res){
     
 });
 
+//EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.render("campgrounds/edit",{campground: foundCampground});
+        }
+    });
+    
+});
+
+//UPDATE CAMPGROUND ROUTE
+router.put("/:id", function(req, res){
+
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            res.redirect("/campgrounds");
+        }else{
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    });
+})
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
