c4560cdf4f149df91831c9f17ca71d72a4f828bd
Shai Aarons <shai.aarons21@gmail.com>
2020-04-26 09:12:30 +0200

    Refactored more + Added association between users and cocomments

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
-seedDB();
+
+//seedDB(); // Seed the database
 
 //PASSPORT CONFIG
 app.use(require("express-session")({
