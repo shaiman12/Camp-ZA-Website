3192043781dfae97dfe579e8d46c26a5c606f84e
Shai Aarons <shai.aarons21@gmail.com>
2020-04-27 09:37:25 +0200

    Added some success and error flash messages

--- routes/comments.js
+++ routes/comments.js
@@ -24,6 +24,7 @@ router.post("/", middleware.isLoggedIn, function(req,res){
         }else{
             Comment.create(req.body.comment,function(err, comment){
                 if(err){
+                    req.flash("error", "Something went wrong");
                     console.log(err);
                 }else{
                     comment.author.id = req.user._id
@@ -31,7 +32,7 @@ router.post("/", middleware.isLoggedIn, function(req,res){
                     comment.save();
                     campground.comments.push(comment);
                     campground.save();
-                    
+                    req.flash("success", "Successfully added comment");
                     res.redirect("/campgrounds/"+campground._id);
                 }
             });
@@ -67,6 +68,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership,  function(req, r
         if(err){
             res.redirect("back");
         }else{
+            req.flash("success", "Comment deleted");
             res.redirect("/campgrounds/"+req.params.id);
         }
     });
