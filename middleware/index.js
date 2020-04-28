587c9604753661535f1615674a51c523142f56b0
Shai Aarons <shai.aarons21@gmail.com>
2020-04-28 16:40:06 +0200

    Added admin abilities

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
                 req.flash("error", "Campground not found");
                 res.redirect("back");
             }else{
-                if(foundCampground.author.id.equals(req.user._id)){
+                if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
                     next();
                 }else{
                     req.flash("error", "You don't have permission to do that");
@@ -33,7 +33,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
             if(err){
                 res.redirect("back");
             }else{
-                if(foundComment.author.id.equals(req.user._id)){
+                if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
                     next();
                 }else{
                     req.flash("error", "You don't have permission to do that");
